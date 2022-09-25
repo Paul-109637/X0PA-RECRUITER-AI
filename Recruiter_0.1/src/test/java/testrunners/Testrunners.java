19a4c60e3a8c42eb8b966054f6cd5c64aa3680bc
Paul-109637 <113929951+Paul-109637@users.noreply.github.com>
2022-09-25 19:16:36 +0530

    Update Testrunners.java

--- Recruiter_0.1/src/test/java/testrunners/Testrunners.java
+++ Recruiter_0.1/src/test/java/testrunners/Testrunners.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="featurefiles/featurefiles.feature",glue="stepdefinitions",
-dryRun=false,plugin={"plugin = { "pretty", "json:target/cucumber.json" ,com.cucumber.listener.ExtentCucumberFormatter:Reports/reports.html"},tags={"@RecruiterLogin,@RecruiterLogout,@CreateNewJob"})
+dryRun=false,plugin={"plugin = { "pretty", "json:target/cucumber-reports/cucumber.json" ,com.cucumber.listener.ExtentCucumberFormatter:Reports/reports.html"},tags={"@RecruiterLogin,@RecruiterLogout,@CreateNewJob"})
 public class Testrunners 
 {
 	
